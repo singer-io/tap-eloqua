63ed77474dd565e66c4ad1226b25c7d0162d1d20
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2018-11-27 10:54:29 -0500

    Update setup.py
    Bumping `requests` dependency in relation to [`CVE 2018-18074`](https://cve.mitre.org/cgi-bin/cvename.cgi?name=2018-18074)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-eloqua',
       py_modules=['tap_eloqua'],
       install_requires=[
           'backoff==1.3.2',
-          'requests==2.19.1',
+          'requests==2.20.0',
           'pendulum==2.0.3',
           'singer-python==5.2.0'
       ],
