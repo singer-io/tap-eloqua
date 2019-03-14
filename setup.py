b40aa203e4c07249cc80e0e488e09e93b30e8058
Dan Mosora <dan@stitchdata.com>
2019-03-14 20:20:09 +0000

    Version 0.4.1, state fix and sync expiration fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-eloqua',
-      version='0.4.0',
+      version='0.4.1',
       description='Singer.io tap for extracting data from the Oracle Eloqua API',
       author='Stitch',
       url='https://singer.io',
