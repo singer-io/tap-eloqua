30b472c06dc5fc004a79c4bdcd7eedc1086e5f78
Dan Mosora <dan@stitchdata.com>
2019-04-11 13:56:24 +0000

    Version 0.6.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-eloqua',
-      version='0.6.2',
+      version='0.6.3',
       description='Singer.io tap for extracting data from the Oracle Eloqua API',
       author='Stitch',
       url='https://singer.io',
