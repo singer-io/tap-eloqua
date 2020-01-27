62ae3a904e025645d9a08082623c13f9bcbfbc8a
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2020-01-27 15:43:04 -0500

    bump to 1.0.0 and add circle file (#27)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-eloqua',
-      version='0.6.6',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Oracle Eloqua API',
       author='Stitch',
       url='https://singer.io',
