290be540cf7a5279648d993b8311d35d84f6ad21
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -1,1 +1,31 @@
-clojure.lang.LazySeq@f88429e7
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-eloqua',
+      version='1.3.1',
+      description='Singer.io tap for extracting data from the Oracle Eloqua API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_eloqua'],
+      install_requires=[
+          'backoff==1.3.2',
+          'requests==2.31.0',
+          'pendulum==2.0.3',
+          'singer-python==5.2.0'
+      ],
+      extras_require={
+          'dev': [
+              'ipdb==0.11'
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-eloqua=tap_eloqua:main
+      ''',
+      packages=['tap_eloqua'],
+      package_data = {
+          'tap_eloqua': ['schemas/*.json'],
+      },
+)
